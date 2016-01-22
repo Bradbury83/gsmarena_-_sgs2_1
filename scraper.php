73dae4b0185edca059c38903a13c6153f6c6961f
Bradbury83 <emobi.adw@gmail.com>
2016-01-22 18:03:41 +0100

    Fork of code from ScraperWiki at https://classic.scraperwiki.com/scrapers/gsmarena_-_sgs2_1/

---
+++ scraper.php
@@ -0,0 +1,19 @@
+<?php
+require 'scraperwiki/simple_html_dom.php';
+
+$html = scraperWiki::scrape("http://www.gsmarena.com/samsung_i9300_galaxy_s_iii-4238.php");
+//print $html . "\n";
+
+$dom = new simple_html_dom();
+$dom->load($html);
+
+foreach($dom->find("div#specs-list tr") as $data)
+{
+    $tds = $data->find("td");
+    $record = array(
+        'NAME' => $tds[0]->plaintext, 
+        'VALUE' => $tds[1]->plaintext
+    );
+    print_r($record);
+}
+?>
